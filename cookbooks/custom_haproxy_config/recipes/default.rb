4c960e08d7e720dcbc958963e9cfb4fe091787f1
Sebastian Maier <sebastian.maier@im-concepts.de>
2012-10-16 14:24:40 +0200

    again bugfix

--- cookbooks/custom_haproxy_config/recipes/default.rb
+++ cookbooks/custom_haproxy_config/recipes/default.rb
@@ -2,6 +2,8 @@
 # Cookbook Name:: custom_haproxy_config
 # Recipe:: default
 #
-execute "update haproxy.cfg" do
-  command "sed -i -e 's/httpclose/http-server-close/' /etc/haproxy.cfg && /etc/init.d/haproxy reload" if File.exist?("/etc/haproxy.cfg")
+if File.exist?("/etc/haproxy.cfg")
+	execute "update haproxy.cfg" do
+	  command "sed -i -e 's/httpclose/http-server-close/' /etc/haproxy.cfg && /etc/init.d/haproxy reload"
+	end
 end

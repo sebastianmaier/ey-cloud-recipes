de0e919c649081086686c65daaba8ca1a6329bce
Sebastian Maier <sebastian.maier@im-concepts.de>
2012-11-20 23:35:43 +0100

    clean release of nginx customizer

--- cookbooks/custom_nginx_cms/recipes/default.rb
+++ cookbooks/custom_nginx_cms/recipes/default.rb
@@ -2,12 +2,21 @@
 # Cookbook Name:: custom_nginx_cms
 # Recipe:: default
 #
-if File.exist?("/etc/nginx/nginx.conf")
-	execute "update nginx.conf" do
-	  command "sed -i -e 's/include \/etc\/nginx\/servers\/\*\.conf;/ \
-				 include \/etc\/nginx\/servers\/cms\.conf; \
-				 include \/etc\/nginx\/servers\/cms_beta\.conf; \
-				 /g' nginx.conf"
-	  command "/etc/init.d/nginx restart"
+if ['app_master','app'].include?(node[:instance_role])
+	if File.exist?("/data/nginx/nginx.conf")
+		execute "update nginx.conf" do
+		  contents = File.read('/data/nginx/nginx.conf')
+		  result = contents.gsub(/include \/etc\/nginx\/servers\/\*\.conf;/, "include /etc/nginx/servers/cms.conf;\ninclude /etc/nginx/servers/cms_beta.conf; ")					 
+		    begin
+			  file = File.open("/data/nginx/nginx.conf", "w")
+			  file.write(result) 
+			rescue IOError => e
+			  #some error occur, dir not writable etc.
+			ensure
+			  file.close unless file == nil
+			end
+		  command "/etc/init.d/nginx restart"
+		  action :run
+		end
 	end
 end

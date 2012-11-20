6b631fa53f478ac34207eff89cabd47c6c5095e8
Sebastian Maier <sebastian.maier@im-concepts.de>
2012-11-20 22:39:37 +0100

    fixed cookbook for custom nginx

--- cookbooks/custom_nginx_cms/recipes/default.rb
+++ cookbooks/custom_nginx_cms/recipes/default.rb
@@ -2,12 +2,14 @@
 # Cookbook Name:: custom_nginx_cms
 # Recipe:: default
 #
-if File.exist?("/etc/nginx/nginx.conf")
-	execute "update nginx.conf" do
-	  command "sed -i -e 's/include \/etc\/nginx\/servers\/\*\.conf;/ \
-				 include \/etc\/nginx\/servers\/cms\.conf; \
-				 include \/etc\/nginx\/servers\/cms_beta\.conf; \
-				 /g' nginx.conf"
-	  command "/etc/init.d/nginx restart"
+if ['app_master','app'].include?(node[:instance_role])
+	if File.exist?("/etc/nginx/nginx.conf")
+		execute "update nginx.conf" do
+		  command "sed -i -e 's/include \/etc\/nginx\/servers\/\*\.conf;/ \
+					 include \/etc\/nginx\/servers\/cms\.conf; \
+					 include \/etc\/nginx\/servers\/cms_beta\.conf; \
+					 /g' nginx.conf"
+		  command "/etc/init.d/nginx restart"
+		end
 	end
 end

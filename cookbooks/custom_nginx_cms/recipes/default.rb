a0fbd3ad375ebce406968e9d42336edaa9c27744
Sebastian Maier <sebastian.maier@im-concepts.de>
2012-11-20 23:04:43 +0100

    debugging

--- cookbooks/custom_nginx_cms/recipes/default.rb
+++ cookbooks/custom_nginx_cms/recipes/default.rb
@@ -4,15 +4,13 @@
 #
 Chef::Log.info "should update nginx.conf"
 if ['app_master','app'].include?(node[:instance_role])
-	if File.exist?("/etc/nginx/nginx.conf")
+	if File.exist?("/data/nginx/nginx.conf")
 		execute "update nginx.conf" do
-		  command "sed -i -e 's/include \/etc\/nginx\/servers\/\*\.conf;/ \
-					 include \/etc\/nginx\/servers\/cms\.conf; \
-					 include \/etc\/nginx\/servers\/cms_beta\.conf; \
-					 /g' /data/nginx/nginx.conf"
-		  contents = File.read('/etc/nginx/nginx.conf')					 
-		  Chef::Log.info contents
+		  command "sed -i -e 's/include \/etc\/nginx\/servers\/\*\.conf;/ test123 /g' /data/nginx/nginx.conf"
 		  Chef::Log.info "hopefuly updated nginx.conf"
+		  contents = File.read('/data/nginx/nginx.conf')					 
+		  Chef::Log.info contents
+
 		  command "/etc/init.d/nginx restart"
 		end
 	end

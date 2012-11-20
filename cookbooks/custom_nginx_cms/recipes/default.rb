aadeeb2bdb3c24896cc5609a2e0cc2aac3613dcb
Sebastian Maier <sebastian.maier@im-concepts.de>
2012-11-20 23:34:54 +0100

    Revert "fixed cookbook for custom nginx"
    This reverts commit 6b631fa53f478ac34207eff89cabd47c6c5095e8.

--- cookbooks/custom_nginx_cms/recipes/default.rb
+++ cookbooks/custom_nginx_cms/recipes/default.rb
@@ -2,14 +2,12 @@
 # Cookbook Name:: custom_nginx_cms
 # Recipe:: default
 #
-if ['app_master','app'].include?(node[:instance_role])
-	if File.exist?("/etc/nginx/nginx.conf")
-		execute "update nginx.conf" do
-		  command "sed -i -e 's/include \/etc\/nginx\/servers\/\*\.conf;/ \
-					 include \/etc\/nginx\/servers\/cms\.conf; \
-					 include \/etc\/nginx\/servers\/cms_beta\.conf; \
-					 /g' nginx.conf"
-		  command "/etc/init.d/nginx restart"
-		end
+if File.exist?("/etc/nginx/nginx.conf")
+	execute "update nginx.conf" do
+	  command "sed -i -e 's/include \/etc\/nginx\/servers\/\*\.conf;/ \
+				 include \/etc\/nginx\/servers\/cms\.conf; \
+				 include \/etc\/nginx\/servers\/cms_beta\.conf; \
+				 /g' nginx.conf"
+	  command "/etc/init.d/nginx restart"
 	end
 end

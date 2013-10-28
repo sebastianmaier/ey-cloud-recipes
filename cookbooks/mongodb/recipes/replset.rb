7f24d33bd96281e02caea0651071b3f426cd214a
sebastian.maier <sebastian.maier@im-concepts.de>
2013-10-28 10:39:40 +0100

    again more logging info

--- cookbooks/mongodb/recipes/replset.rb
+++ cookbooks/mongodb/recipes/replset.rb
@@ -5,7 +5,7 @@ mongo_arbiter="na"
 if @node[:mongo_replset]
   mongo_nodes = @node[:utility_instances].select { |instance| instance[:name].match(/^mongodb_repl#{@node[:mongo_replset]}/) }
 
-  # Chef::Log.info "Setting up Replica set: #{node[:mongo_replset]} \n mongo_nodes: #{mongo_nodes.inspect} \n executing on node #{@node[:name]}"
+  Chef::Log.info "Setting up Replica set: #{node[:mongo_replset]} \n mongo_nodes: #{mongo_nodes.inspect} \n executing on node #{@node[:name]}"
 
   if (@node[:name].match(/_1$/) && (mongo_nodes.length == 3 || (mongo_nodes.length ==2 && mongo_arbiter.length > 3)))
     Chef::Log.info "setup replset js at least once" 
